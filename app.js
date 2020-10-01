6ab00f62b22395b9725948ed20437d088b1c52e3
Juan Vargas <juandavc12@hotmail.com>
2020-10-01 18:57:53 -0500

    Visitantes

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongoose', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+ });
+
+ mongoose.connection.on("error", function (error){
+     if (error){
+         return console.log(error);
+     };
+ })
+
+ const VisitorSchema = mongoose.Schema ({
+    date: Date,
+    name: String,
+ });
+
+ const VisitorModel = mongoose.model("Visitor", VisitorSchema);
+
+ app.get("/", (req, res) => {
+    const { name } = req.query;
+
+    VisitorModel.create ({
+        date: new Date(),
+        name: name ? name : 'Anónimo',
+    });
+
+    res.send("<h1>El visitante fue almacenado con éxito</h1>");
+ });
+
+ app.listen(3000, () => {
+     console.log("server listening on port: 3000")
+ });
